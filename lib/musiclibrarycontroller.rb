7f105746b754c9ab4a9c3ad249bdd12acc53c219
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-22 20:56:40 +0000

    Automatically backed up by Learn

--- lib/musiclibrarycontroller.rb
+++ lib/musiclibrarycontroller.rb
@@ -21,6 +21,11 @@ class MusicLibraryController
       puts "What would you like to do?"
       input = gets.chomp
     end
+  end
 
+  def list_songs
+    Song.all.sort {|a,b| a.name <=> b.name}.each.with_index(1) do |song, i|
+      puts "#{i}. #{song.artist.name} - #{song.name} - #{song.genre.name}"
+    end
   end
 end
